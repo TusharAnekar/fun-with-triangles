2edffe1ce1a24a4782277de8f587cb6fb4bcd2fa
Tushar Anekar <92424004+TusharAnekar@users.noreply.github.com>
2022-07-29 17:21:06 +0530

    validation check for fields

---
+++ hypotenues.js
@@ -0,0 +1,20 @@
+const sideA = document.querySelector("#side-a");
+const sideB = document.querySelector("#side-b");
+const checkHypotenues = document.querySelector("#check-hypotenues");
+const output = document.querySelector("#output")
+
+checkHypotenues.addEventListener ("click", () => {
+    
+    if (sideA.value < 0 || sideB.value < 0) {
+        output.innerText = "Please enter positive values in both side A and B."
+    } else if (sideA.value === "" || sideB.value === "") {
+        output.innerText = "Please enter values in both sides A and B"
+    } else {
+        var hypotenues =
+        Math.sqrt((sideA.value*sideA.value)+(sideB.value*sideB.value));
+        
+        output.innerText = "The length of the hypotenuse is "+hypotenues+"cm";
+    }
+    console.log(sideA.value, sideB.value)
+    
+});
